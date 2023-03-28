74aa29a0d7f4d36407fbaa5dcc1add568afd3c17
Alfred Syatsukwa <alfredsyatsukwa@gmail.com>
2023-03-28 13:35:52 -0700

    Corrections for betty style

--- 0-printf.c
+++ 0-printf.c
@@ -6,13 +6,14 @@
  *
  * Return: the number of characters printed
  */
-int _printf(const char * const format, ...) {
-	int c;
+int _printf(const char *format, ...)
+{
+	int c, count = 0, i;
 	char ch;
 	char *s;
 	va_list args;
+
 	va_start(args, format);
-	int count = 0, i;
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
@@ -21,31 +22,28 @@ int _printf(const char * const format, ...) {
 			if (format[i] == 'c')
 			{
 				c = va_arg(args, int);
-				write(1, &c, 1);
-				++count;
+				write(1, &c, 1), ++count;
 			} else if (format[i] == 's')
 			{
 				s = va_arg(args, char*);
 				while (*s != '\0')
 				{
-					write(1, s++, 1);
-					++count;
+					write(1, s++, 1), ++count;
 				}
-			} 
+			}
 			else if (format[i] == '%')
 			{
 				ch = '%';
-				write(1, &ch, 1);
-				++count;
+				write(1, &ch, 1), ++count;
 			}
-		} else {
-			write(1, &format[i], 1);
-			++count;
+		}
+		else
+		{
+			write(1, &format[i], 1), ++count;
 		}
 	}
 	ch = '\n';
-	write(1, &ch, 1);
-	count++;
+	write(1, &ch, 1), count++;
 	va_end(args);
-	return count;
+	return (count);
 }
